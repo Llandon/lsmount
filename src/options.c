8229b0d99b4b5f29019dedcb8c51f66bcd0c5577
Andreas Schwarz <a.schwarz_dev@dntw.de>
2019-01-05 18:14:57 +0100

    update version string

--- src/options.c
+++ src/options.c
@@ -272,7 +272,7 @@ void usage (int status) {
 	       "all short options can be inverted by using the uppercase letter,\n"
 	       "the longopts can be inverted by adding dont- in front.\n"
 	       "\n"
-	       "Version: v0.2.2\n"
+	       "Version: v0.2.3\n"
 	       "License: ISC\n")
 	);
 	free(to_skip);
